652ee80c19453593660003f5e2e60c234df433a4
lrh <153011490@qq.com>
2019-07-09 00:22:47 +0800

    update

--- practice/section-2/practice-3.js
+++ practice/section-2/practice-3.js
@@ -1,5 +1,39 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result=[];
+  var arrLength=collection.length;
+  var count=0;
+  for(var i=0;i<arrLength;i++){
+    if(i<arrLength-1&&collection[i].substr(0,1)==collection[i+1].substr(0,1)){
+      if(collection[i].length>1){
+        var str=collection[i];
+        if(collection[i].indexOf("-")){
+          count+=parseInt(str.substring(2));
+          continue;
+        }
+        if(collection[i].indexOf("[")){
+          count+=parseInt(str.substring(2,str.length-1));
+          continue;
+        }
+      }else{
+        count++;
+      }
+    }else{
+      if(collection[i].length>1){
+        if(collection[i].indexOf("-")||collection[i].indexOf(":")){
+          var str=collection[i];
+          count+=parseInt(str.substring(2));
+        }
+      }else{
+        count++;
+      }
+      var temp={name:'',summary:0};
+      temp.name=collection[i].charAt(0);
+      temp.summary=count;
+      result.push(temp);
+      count=0;
+    }
+  }
+  return result;
 }
