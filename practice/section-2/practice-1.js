e5f0a861fc63530c4fccdea1c1ec68fe021bc606
lrh <153011490@qq.com>
2019-07-08 23:53:16 +0800

    update code

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,20 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  const result=[];
+  var arrLength=collection.length;
+  var count=0;
+  for(var i=0;i<arrLength;i++){
+    if(i<arrLength-1&&collection[i]==collection[i+1]){
+      count++;
+    }else{
+      count++;
+      var temp={key:'',count:0};
+      temp.key=collection[i];
+      temp.count=count;
+      result.push(temp);
+      count=0;
+    }
+  }
+  return result;
 }
--- practice/section-2/practice-2.js
+++ practice/section-2/practice-2.js
@@ -1,5 +1,34 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var result=[];
+  var arrLength=collection.length;
+  var count=0;
+  for(var i=0;i<arrLength;i++){
+    if(i<arrLength-1&&collection[i].substr(0,1)==collection[i+1].substr(0,1)){
+      if(collection[i].length>1){
+        if(collection[i].indexOf("[")){
+          var str=collection[i];
+          count+=parseInt(str.substring(2,str.length-2));
+        }
+      }else{
+        count++;
+      }
+    }else{
+      if(collection[i].length>1){
+        if(collection[i].indexOf("-")){
+          var str=collection[i];
+          count+=parseInt(str.substring(2));
+        }
+      }else{
+        count++;
+      }
+      var temp={key:'',count:0};
+      temp.key=collection[i].charAt(0);
+      temp.count=count;
+      result.push(temp);
+      count=0;
+    }
+  }
+  return result;
 }
